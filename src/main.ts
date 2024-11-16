7b16a108aae21bc404246bed3b0e5027db6e8793
Antonio Lombardi <antolomb03@pop-os.station>
2024-11-16 21:18:00 +0100

    Commit iniziale in cui ho testato : Data Binding => Interpolation {{}}, propertyBinding [], two way data binding [(ngModel)], event Binding, Directives => Structural, Attribute (ngIf,ngFor || ngClass, ngStyle)

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,4 @@
+//Primo file che viene eseguito una volta avviato il progetto
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
