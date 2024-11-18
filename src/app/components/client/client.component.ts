064e16bbb1a76b88dd595a356e464d896c216fc8
Antonio Lombardi <antolomb03@pop-os.station>
2024-11-18 13:59:06 +0100

    Operazioni Crud Sul client, aggiornato quindi il client.component.ts

--- src/app/components/client/client.component.ts
+++ src/app/components/client/client.component.ts
@@ -1,12 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { Client } from '../../model/class/Class';
+import { FormsModule } from '@angular/forms';
+import { ClientService } from '../../services/client.service';
+import { APIResponseModel } from '../../model/interface/role';
 
 @Component({
   selector: 'app-client',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './client.component.html',
   styleUrl: './client.component.css'
 })
-export class ClientComponent {
+export class ClientComponent implements OnInit{
+  
+  clientObj: Client = new Client();
+  
+  clientList: Client[] = [];
+  clientService = inject(ClientService);
+  
+  ngOnInit(): void {
+    this.loadClient();
+  }
+  
+  onSaveClient() {
+    this.clientService.addUpdate(this.clientObj).subscribe((res: APIResponseModel)=>{
+      if(res.result) {
+        alert("Client Created Success")
+        this.loadClient();
+        this.clientObj = new Client();
+      }else {
+        alert(res.message)
+      }
+    })
+  }
+  
+  loadClient() {
+    this.clientService.getAllClients().subscribe((res: APIResponseModel)=>{
+      this.clientList = res.data;
+    })
+  }
+  
+  reset() {
+    this.clientObj= new Client();
+  }
+  
+  onDelete(ClientId: number) {
+    const isDelete = confirm("Are you sure want to delete");
+    if(isDelete) {
+      this.clientService.deleteClientById(ClientId).subscribe((res: APIResponseModel) => {
+        if(res.result) {
+          alert("Client Delete Success");
+          this.loadClient();
+        } else {
+          alert(res.message);
+        }
+        
+      });
+    }
+  }
 
+  onEdit(data: Client) {
+    this.clientObj = data;
+  }
+
+  
 }
